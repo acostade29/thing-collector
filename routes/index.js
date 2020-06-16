dd48bf959056b0e2b610bc9125132a90589f80d1
acostade <acostade@gmail.com>
2020-06-16 10:14:55 -0500

    new car

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/cars2', function(req, res) {
+  res.render('cars2')
+})
+
+
 module.exports = router;
